ae0ad779386dd87178f3126058867221c62d9ce8
amaraa <amaraa8525@gmail.com>
2020-06-17 21:52:15 +0800

    finence modul hiij ehelsen orlogo zarlaga hadgalsan

--- app.js
+++ app.js
@@ -1,30 +1,87 @@
 //Дэлгэцтэй ажиллах контроллер
 var uiController = (function() {
-   
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addBtn:'.add__btn'
+    };
+
+   return {
+       getInput: function(){
+           return {
+           type: document.querySelector(DOMstrings.inputType).value,
+           description: document.querySelector(DOMstrings.inputDescription).value,
+           value: document.querySelector(DOMstrings.inputValue).value
+       }
+   },
+   getDOMstrings: function(){
+       return DOMstrings;
+   }
+}
 })();
 //Сахүүтэй ажиллах контроллер
 var financeController = (function() {
- 
+ var Income = function(id, description, value) {
+     this.id = id;
+     this.description = description;
+     this.value = value
+ }
+
+ var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value
+}
+
+var data = {
+    allItems: {
+        inc: [],
+        exp: []
+    },
+
+    totals: {
+        inc: 0,
+        exp: 0
+    }
+}
+
+
 })();
 //Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
-
     var ctrlAddItem = function(){
         //дэлгэцнээс оруулсан өгөгдлийг олж авна
-    console.log("дэлгэцнээс өгөгдөл авах хэсэг");
+    console.log(uiController.getInput());
     // олж авсан өгөгдлөө санхүүгийн контроллерт дамжуулан тэнд хадгална
     //олж авсан өгөгдлөө тохирох хэсэгт дэлгэцэнд гаргана
     //төсвийг тооцно
     // эцийн үдэгдэл тооцоог дэлгэцэнд гаргана
     }
 
-    document.querySelector('.add__btn').addEventListener("click", function(){
-        ctrlAddItem();
-    });
+    var setupEventListeners = function(){
 
-    document.addEventListener("keypress", function(event){
-        if(event.keyCode === 13 || event.which === 13) {ctrlAddItem()};
-    });
+        var DOM = uiController.getDOMstrings();
+
+        document.querySelector(DOM.addBtn).addEventListener("click", function(){
+            ctrlAddItem();
+        });
     
+        document.addEventListener("keypress", function(event){
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem()
+            };
+        });
+    }
+
+    return {
+        init: function() {
+        console.log('web ajilllaj ehellee ...............');
+        setupEventListeners();
+        }
+    }
     
 })(uiController, financeController);
+
+appController.init();
